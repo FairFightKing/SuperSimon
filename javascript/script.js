6ae3a38591b79c0d689d0112cd23c69360c2e8e1
FairFightKIng <sosthenefruchard@gmail.com>
2019-11-19 16:09:37 +0100

    Result verification

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,23 @@
 class Computer{
-    constructor(x = 4){
-        this.x = x;
+    constructor(inputsPerTurn = 4){
+        this.inputsPerTurn = inputsPerTurn;
         this.result = "";
         this.init();
     }
     init(){
         let i;
-        for(i = 0; i < this.x; i++) {
-            let test = Math.floor(Math.random() * Math.floor(7));
+        for(i = 0; i < this.inputsPerTurn; i++) {
+            let test = Math.floor(1 + Math.random() * Math.floor(4));
             this.result += test;
         };
         console.log(this.result);
     }
+    roundEND(userInputs){
+        this.result == userInputs ? console.log('win') : console.log('loose');
+    }
 
 }
 
-const computer1 = new Computer(6);
+const computer1 = new Computer(1);
+
+computer1.roundEND(4);
