c5605a61037ae0f2b179d8fcf2a2d084b935f816
FairFightKIng <sosthenefruchard@gmail.com>
2019-11-19 22:52:55 +0100

    relative interval speed + using array instead of string

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,48 @@
-class Computer{
+class GameMaster{
     constructor(inputsPerTurn = 4){
         this.inputsPerTurn = inputsPerTurn;
-        this.result = "";
+        this.result = [];
+        this.userString = "";
+        this.userEvent = 0;
+        this.speedInterval = 2000;
+        this.userStreak = 0;
     }
-    generator(){
+    roundSTART(){
         let x = 0;
         let rowsGenerator = window.setInterval( () => {
             this.inputsPerTurn === x++ ? window.clearInterval(rowsGenerator) : console.log('not finished yet');
             let test = Math.floor(1 + Math.random() * Math.floor(4));
-            this.result += test;
+            this.result.push(test);
            // document.querySelector('input[value="'+test+'"]').value = "ohlala";
             console.log(x);
-        },  1000);
+        },  this.speedInterval);
         console.log(this.result);
     }
 
-    roundEND(userInputs){
-        this.result == userInputs ? console.log('win') : console.log('loose');
+    roundEND(roundResult){
+        this.userString = "";
+        this.userEvent = 0;
+        if(roundResult){
+            this.speedInterval -= 220;
+            this.userStreak += 1;
+        }else{
+            this.userStreak = 0;
+        }
     }
 
-
+    userClick(eventValue){
+        if(this.result[this.userEvent] === eventValue){
+            this.userEvent++;
+        }else{
+            this.roundEND(false);
+            return;
+        }
+        if(this.userEvent === this.inputsPerTurn){
+            this.roundEND(true);
+        }
+    }
 }
 
-const computer1 = new Computer(4);
-computer1.generator();
-computer1.roundEND(4);
+const GM = new GameMaster(4);
+GM.roundSTART();
+GM.roundEND(4);
