40ccfba528bb76b52ba8575f6ac8ba707574c3d2
FairFightKIng <sosthenefruchard@gmail.com>
2019-11-19 16:34:52 +0100

    Set interval wich will define the computer string and the event of modification of the inputs

--- javascript/script.js
+++ javascript/script.js
@@ -2,22 +2,26 @@ class Computer{
     constructor(inputsPerTurn = 4){
         this.inputsPerTurn = inputsPerTurn;
         this.result = "";
-        this.init();
     }
-    init(){
-        let i;
-        for(i = 0; i < this.inputsPerTurn; i++) {
+    generator(){
+        let x = 0;
+        let rowsGenerator = window.setInterval( () => {
+            this.inputsPerTurn === x++ ? window.clearInterval(rowsGenerator) : console.log('not finished yet');
             let test = Math.floor(1 + Math.random() * Math.floor(4));
             this.result += test;
-        };
+           // document.querySelector('input[value="'+test+'"]').value = "ohlala";
+            console.log(x);
+        },  1000);
         console.log(this.result);
     }
+
     roundEND(userInputs){
         this.result == userInputs ? console.log('win') : console.log('loose');
     }
 
+
 }
 
-const computer1 = new Computer(1);
-
+const computer1 = new Computer(4);
+computer1.generator();
 computer1.roundEND(4);
