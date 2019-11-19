b1bbf5b5c6446f5549a23134ec477e6db6546235
FairFightKIng <sosthenefruchard@gmail.com>
2019-11-19 23:12:46 +0100

    fixed array generator and inputs per round value placement ajusted

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,24 @@
 class GameMaster{
-    constructor(inputsPerTurn = 4){
-        this.inputsPerTurn = inputsPerTurn;
+    constructor(){
+        this.inputsPerTurn = 0;
         this.result = [];
         this.userString = "";
         this.userEvent = 0;
         this.speedInterval = 2000;
-        this.userStreak = 0;
+        this.userRoundStreak = 0;
+        this.userInputStreak = 0;
     }
-    roundSTART(){
+    roundSTART(inputsPerTurn = 4){
+        this.inputsPerTurn = inputsPerTurn;
         let x = 0;
         let rowsGenerator = window.setInterval( () => {
-            this.inputsPerTurn === x++ ? window.clearInterval(rowsGenerator) : console.log('not finished yet');
+            this.inputsPerTurn - 1 === x++ ? window.clearInterval(rowsGenerator) : console.log('not finished yet');
             let test = Math.floor(1 + Math.random() * Math.floor(4));
             this.result.push(test);
            // document.querySelector('input[value="'+test+'"]').value = "ohlala";
             console.log(x);
+            console.log(this.result);
         },  this.speedInterval);
-        console.log(this.result);
     }
 
     roundEND(roundResult){
@@ -24,25 +26,26 @@ class GameMaster{
         this.userEvent = 0;
         if(roundResult){
             this.speedInterval -= 220;
-            this.userStreak += 1;
+            this.userRoundStreak += 1;
         }else{
-            this.userStreak = 0;
+            this.userRoundStreak = 0;
+            this.userInputStreak = 0;
+            this.speedInterval = 2000;
         }
     }
 
     userClick(eventValue){
         if(this.result[this.userEvent] === eventValue){
             this.userEvent++;
-        }else{
+            this.userInputStreak++;
+            if(this.userEvent === this.inputsPerTurn) {
+                this.roundEND(true);
+            }
+        } else {
             this.roundEND(false);
-            return;
-        }
-        if(this.userEvent === this.inputsPerTurn){
-            this.roundEND(true);
         }
     }
 }
 
-const GM = new GameMaster(4);
-GM.roundSTART();
-GM.roundEND(4);
+const GM = new GameMaster();
+GM.roundSTART(4);
