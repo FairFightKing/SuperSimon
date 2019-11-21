6e50cffc85d76f5b71d0e5555082c38d04950dd0
FairFightKIng <sosthenefruchard@gmail.com>
2019-11-21 14:26:36 +0100

    GameMaster working when using userClick via the console. Still need to remember to retype the first entry of the array

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,18 @@
 class GameMaster{
     constructor(){
-        this.inputsPerTurn = 1;
+        this.inputsPerTurn = 0;
         this.result = [];
-        this.userInputs = "";
         this.userEvent = 0;
-        this.speedInterval = 2000;
+        this.speedInterval = 2220;
         this.userRoundStreak = 0;
         this.userInputStreak = 0;
     }
-    roundSTART(){
+
+    // Start a round, make the input per turn grow and generate a new number to play and make interval faster
+    roundSTART = () => {
         let x = 0;
+        this.inputsPerTurn++;
+        this.speedInterval -= this.speedInterval === 240 ? 0 : 220;
         let rand = Math.floor(1 + Math.random() * Math.floor(4));
         this.result.push(rand);
         let rowsGenerator = window.setInterval( () => {
@@ -19,29 +22,53 @@ class GameMaster{
             console.log(this.result);
         },  this.speedInterval);
     }
-
-    roundEND(roundResult){
-        this.userInputs = "";
+    // Used whenever the user loose or has clicked as many time as needed roundResult is either true or false
+    roundEND = (roundResult) => {
         this.userEvent = 0;
         if(roundResult){
             this.speedInterval -= 220;
             this.userRoundStreak += 1;
+            window.setTimeout( () => {
+               this.roundSTART();
+               console.log('go win');
+            }, 3000);
+            console.log('Next round in 3s');
         }else{
+            console.log('Game reseting,');
             this.userRoundStreak = 0;
             this.userInputStreak = 0;
-            this.speedInterval = 2000;
+            this.inputsPerTurn = 0;
+            this.result = [];
+            this.speedInterval = 2220;
+            // Vous avez perdu
+            window.setTimeout( () => {
+                this.roundSTART();
+                console.log('go loose');
+            }, 3000);
+            console.log('Next round in 3s');
         }
     }
 
-    userClick(eventValue){
-        if(this.result[this.userEvent] === eventValue){
+    userClick = ($eventValue) => {
+        console.log("event value :" + $eventValue);
+        console.log("GameMaster value :" + this.result[this.userEvent]);
+        console.log("Array index value :" + this.userEvent);
+        console.log('Input Streak : ' + this.userInputStreak);
+        console.log('Input PER TURN : ' + this.inputsPerTurn);
+        if(this.result[this.userEvent] === $eventValue){
+            console.log('win');
             this.userEvent++;
             this.userInputStreak++;
             if(this.userEvent === this.inputsPerTurn) {
+                console.log('you WIN');
                 this.roundEND(true);
+                // display a green light and a win sound
             }
         } else {
+            console.log('you loose');
             this.roundEND(false);
+            // display a red light and a loose sound plus a scoreboard
+
         }
     }
 }
