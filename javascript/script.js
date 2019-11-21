9030aab6a823fe080824f361030b75edce80af91
FairFightKIng <sosthenefruchard@gmail.com>
2019-11-21 16:03:57 +0100

    everything works like itended, need to do the DOM now

--- javascript/script.js
+++ javascript/script.js
@@ -12,7 +12,7 @@ class GameMaster{
     roundSTART = () => {
         let x = 0;
         this.letUserPLay(false);
-        // add 1 input needed to complete the turn
+        // add 1 input needed to complete the turn 0
         this.inputsPerTurn++;
         // generate a random number between 1 and 4
         let rand = Math.floor(1 + Math.random() * Math.floor(4));
@@ -21,18 +21,17 @@ class GameMaster{
         // Make the inputs play the "song" in a specific interval
         let rowsGenerator = window.setInterval( () => {
             // stop the Interval when all the key are played
-            if(this.inputsPerTurn - 1 === x++ ){
+            if(this.inputsPerTurn === x ){
              window.clearInterval(rowsGenerator);
              this.letUserPLay(true);
-            } else {
-                // do nothing, just let user its not done yet
-                console.log('not finished yet');
-            };
+            }
             // Make the input glow to show the player wich key are being played
-            /* debug
+            let inputPLaying = this.result[x];
+            this.makeInputsInteract(inputPLaying, true);
             console.log(x);
             console.log(this.result);
-             */
+            console.log("Input wich will be played : " + inputPLaying);
+            x++;
         },  this.speedInterval);
     }
     // Used whenever the user loose or has clicked as many time as needed roundResult is either true or false
@@ -73,12 +72,14 @@ class GameMaster{
             console.log('win');
             this.userEvent++;
             this.userInputStreak++;
+            this.makeInputsInteract($eventValue, true);
             if(this.userEvent === this.inputsPerTurn) {
-                console.log('you WIN');
+                console.log('you win the round');
                 this.roundEND(true);
                 // display a green light and a win sound
             }
         } else {
+            this.makeInputsInteract($eventValue, false);
             console.log('you loose');
             this.roundEND(false);
             // display a red light and a loose sound plus a scoreboard
@@ -92,6 +93,16 @@ class GameMaster{
         } else {
             // Make the inputs accessible to the player when the GM finished playing
         }
+    }
+
+    makeInputsInteract($input, correct){
+        // When the user or GM click an input
+        if(correct){
+            // make the $input glow white and play the note
+        } else {
+            // make the $input glow red and play a false note
+        }
+        console.log("input dans makeinput : " + $input);
     }
 }
 
